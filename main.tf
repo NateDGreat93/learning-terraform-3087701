39658e85c43d58da1cde593a9dd2051baefe4beb
NateDGreat93 <149034454+NateDGreat93@users.noreply.github.com>
2024-03-07 21:14:35 -0500

    List ALB

--- main.tf
+++ main.tf
@@ -72,13 +72,13 @@ module "alb" {
     }
   ] 
   
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
+  listeners = {
+    http = {
+      port     = 80
+      protocol = "HTTP"
       target_group_index = 0
     }
-  ]
+  }
 
 
   tags = {
